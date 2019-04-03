6fa0b95709a51396e1b6d5326481caa3f7037975
Brooke Wolford <bwolford@hunt.sph.umich.edu>
2019-04-03 15:59:00 -0400

    Add functionality to make a column with inverse normalization

--- sumGRS.py
+++ sumGRS.py
@@ -14,6 +14,9 @@ import os
 import sys
 import argparse
 import glob
+import numpy as np
+import pandas as pd
+import scipy.stats as ss
 
 ###########################
 ##### PARSE ARGUMENTS ####
@@ -23,7 +26,8 @@ def get_settings():
     parser.add_argument("-s","--sample_file",help="File with FID and IID of the samples you expect to calcualte GRS for",required=True,type=str)
     parser.add_argument("-c","--config",help="Config file with new line delimited list of all the file names to merge",required=True,type=str)
     parser.add_argument("-o","--output",help="Output prefix. Will have .txt appended",required=True,type=str)
-    parser.set_defaults(chrom=True)
+    parser.add_argument("-i","--invNorm",help="Will print additional column with inverse normalized score",action='store_true')
+    parser.set_defaults(chrom=True,invNorm=False)
     args=parser.parse_args()
     return args
 
@@ -110,23 +114,88 @@ def merge(fl,sl):
                         
     return(ddict)
 
-def output(o,d,list_size):
-    
-    #open output file
+def output(o,d,list_size,inorm):
+
     outname=".".join([o,"txt"])
     out_file=open(outname,"w")
-    out_file.write("\t".join(["IID","FID","GPS\n"]))
+    if inorm:
+        #initialize lists for inorm
+        GRS_list=[]
+        id_list=[]
+        out_file.write("\t".join(["IID","FID","GPS","invNorm_GPS\n"]))
+    else:
+        out_file.write("\t".join(["IID","FID","GPS\n"]))
+
     #sum across the nested dictionary to get 1 value per ID
     for ids in d.keys():
         if len(d[ids]) != list_size: #check that all sub sums are represented 
             print >> sys.stderr, "%s does not have expected number (%d) of sub-chunks to sum\n"  % (ids,list_size)
         GRS=sum(float(sub_sum) for sub_sum in d[ids])
-        id_1,id_2=ids.split(".")
-        out_file.write("\t".join([id_1,id_2,str(GRS)])+"\n")
+        #if we want to inverse normalize just save data to ordered lists
+        if inorm:
+            GRS_list.append(GRS)
+            id_list.append(ids)
+        #otherwise write to file
+        else:
+            id_1,id_2=ids.split(".")
+            out_file.write("\t".join([id_1,id_2,str(GRS)])+"\n")
 
+    #now we inverse normalize and write out data using lists 
+    if inorm:
+        trans_GRS_list=rank_INT(GRS_list)
+        for i in range(len(trans_GRS_list)):
+            id_1,id_2=id_list[i].split(".")
+            out_file.write("\t".join([id_1,id_2,str(GRS_list[i]),str(trans_GRS_list[i])])+"\n")
+            
     return 0
 
+
+#Code adapted from https://github.com/edm1/rank-based-INT/blob/master/rank_based_inverse_normal_transformation.py
+def rank_INT(GRS_list,c=3.0/8,stochastic=True):
+    """
+    Perform rank-based inverse normal transformation on pandas series.
+        If stochastic is True ties are given rank randomly, otherwise ties will
+        share the same value. NaN values are ignored.
+        Args:
+            param1 (pandas.Series):   Series of values to transform
+            param2 (Optional[float]): Constand parameter (Bloms constant)
+            param3 (Optional[bool]):  Whether to randomise rank of ties
+        
+        Returns:
+            pandas.Series
+    """
+    #set seed
+    np.random.seed(1234)
     
+    GRS_series=pd.Series(GRS_list)
+    orig_index=GRS_series.index
+    GRS_series=GRS_series.loc[~pd.isnull(GRS_series)] #drop NAN
+
+    #Get ranks
+    #Ties are determined by their position in the series 
+    if stochastic==True:
+        GRS_series=GRS_series.loc[np.random.permutation(GRS_series.index)]
+        rank=ss.rankdata(GRS_series,method="ordinal")
+    #Ties are averaged
+    else:
+        rank=ss.rankdata(GRS_series,method="average")
+
+    #Convert numpy array back to series
+    rank=pd.Series(rank,index=GRS_series.index)
+
+    #Convert rank to normal distribution
+    transformed=rank.apply(rank_to_normal,c=c,n=len(rank))
+
+    return transformed[orig_index]
+
+
+def rank_to_normal(rank, c, n):
+    # Standard quantile function
+    x = (rank - c) / (n - 2*c + 1)
+    return ss.norm.ppf(x)
+    
+    
+
 #########################
 ########## MAIN #########
 #########################
@@ -146,7 +215,7 @@ def main():
     data=merge(file_list,sample_list)
 
     #write output
-    output(args.output,data,len(file_list))
+    output(args.output,data,len(file_list),args.invNorm)
 
     
 
