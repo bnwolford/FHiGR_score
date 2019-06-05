e5cc1c8c5299b6db66015613f40f6db920412437
Brooke Wolford <bwolford@umich.edu>
2019-06-06 00:39:22 +0200

    labels on corrr plot

--- FHiGRS.R
+++ FHiGRS.R
@@ -624,7 +624,7 @@ for (i in 1:size){ #across q-quantiles
   var_list<-c(covar,strat_col,fhigrs_col,grs_col)
   mydata.rcorr<-qsub %>% select(var_list) %>% as.matrix() %>% rcorr()
   row.names(mydata.rcorr$r)<-c("sex","birthyear","batch","PC1","PC2","PC3","PC4","FH","FHIGRS","GRS") #TO DO: generalize
-  names(mydata.rcorr$r)<-c("sex","birthyear","batch","PC1","PC2","PC3","PC4","FH","FHIGRS","GRS") #TO DO: generalize
+  colnames(mydata.rcorr$r)<-c("sex","birthyear","batch","PC1","PC2","PC3","PC4","FH","FHIGRS","GRS") #TO DO: generalize
   pdf_fn<-paste(sep=".",out,quantiles[i],"FHIGRS_correlation.pdf")
   pdf(file=pdf_fn,height=4,width=6,useDingbats=FALSE)
   print(corrplot(mydata.rcorr$r,type="lower"))
